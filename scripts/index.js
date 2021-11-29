9b7d973679bedc6042ce57c27a9c2121d7c315ba
vitaly lukyanchenko <vit123@list.ru>
2021-11-29 12:25:19 +0200

    adds hide details and icons selection markup

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,35 @@
-var thumbnails = document.querySelectorAll(".thumbnails-anchore");
-var detailsImage = document.querySelector(".details-image");
-var detailsTitle = document.querySelector(".details-title");
+const thumbnails = document.querySelectorAll(".thumbnails-anchore");
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector(".details-title");
+const mainContent = document.querySelector(".main-class");
+// тип данных let нужен для того, чтобы можно было менять значение переменной
+let selectedItem = thumbnails[0].parentElement;
+const HIDDEN = "hidden";
+const SELECTED ="selected"
+
 thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function(event) {
         event.preventDefault();
+        showDetails();
         setDetails(thumbnail);
-        })     
-    })
+        selectItem(thumbnail);
+    }) 
+})
 function setDetails(thumbnail) {
     detailsImage.src = thumbnail.href;
     // получение контента из элемента делается через getter
     detailsTitle.textContent = thumbnail.getAttribute("data-detailed-title");
+}
+function showDetails() {
+    // методы группы classList не требуют указания точки (.) перед названием класса
+    mainContent.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainContent.classList.add(HIDDEN);
+    selectedItem.classList.remove(SELECTED);
+}
+function selectItem(thumbnail) {
+    selectedItem.classList.remove(SELECTED);
+    selectedItem = thumbnail.parentElement;
+    selectedItem.classList.add(SELECTED);
 }
